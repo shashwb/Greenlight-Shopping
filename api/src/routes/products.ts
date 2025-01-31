f6e9f6d89dcd2af5aa9c5bc834eb1456a238aa7b
Seth <sethbalodi@gmail.com>
2025-01-31 15:36:43 -0500

    fix: cors issues caused by docker-compose networking fixed

--- api/src/routes/products.ts
+++ api/src/routes/products.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import axios from "axios";
 
 const router = express.Router();
 import __mock__data from "../db/db.ts";
 
+console.log("inside the productsRouter!, __mock__data: ", __mock__data);
+
 // Route to get all posts from JSON Server
 router.get("/", async (req, res) => {
+  console.log("products => router!!!!!");
   try {
     /** mock reponse data for now  */
     const response = {
@@ -16,7 +18,7 @@ router.get("/", async (req, res) => {
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching posts:", error);
-    res.status(500).send("Error fetching posts test");
+    res.status(500).send("Error fetching posts test DOES THIS SHOW UP?");
   }
 });
 
--- api/src/server.ts
+++ api/src/server.ts
@@ -4,8 +4,10 @@ import productsRouter from "./routes/products.ts";
 import cors from "cors";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const app = express();
+
+console.log("server.ts has restarted! PORT: ", PORT);
 
 /** middlewares */
 app.use(cors());
@@ -14,6 +16,15 @@ app.use(express.json());
 /** healthcheck */
 app.get("/", (_req, res) => {
   res.send("Welcome to the Greenlight API");
+});
+
+app.get("/test-cors", (req, res) => {
+  console.log("TEST CORS TEST CORS TEST CORS");
+  res.send(
+    res.json({
+      message: "Test CORS",
+    })
+  );
 });
 
 // 'product' controller -> currently mocked
