03224e05c54e1a350b35e10d2bf89bded4ddfed4
Seth <sethbalodi@gmail.com>
2025-02-01 12:49:32 -0500

    feat: implemented /orders flow to connect to related 'User' and 'Product' table values

--- api/src/routes/orders.ts
+++ api/src/routes/orders.ts
@@ -10,21 +10,56 @@ interface User {
   highHorse: number;
 }
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  description: string;
+  characteristics: string[];
+  sustainabuyScore: number;
+}
+
 interface Order {
   id: number;
   createdAt: Date;
   userId: number;
+  user: User;
+  items: OrderProduct[];
+}
+
+interface OrderProduct {
+  id: number;
+  quantity: number;
+  product: Product;
 }
 
 /** get all past orders for all users (should be able to filter by user) */
 router.get("/", async (_req: Request, res: Response) => {
   try {
     /** query all orders associated with that user's ID */
-    const orders: Order[] = await prisma.order.findMany({
+    const orders: any = await prisma.order.findMany({
       // also fetch the related items for reach order
       include: {
-        user: true,
-        items: true,
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+        items: {
+          include: {
+            product: {
+              select: {
+                name: true,
+                price: true,
+                imageUrl: true,
+                description: true,
+                sustainabuyScore: true,
+              },
+            },
+          },
+        },
       },
     });
 
